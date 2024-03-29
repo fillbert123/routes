9e506bb4a6c880c4bab852136cbe16875c8d9f48
Fillbert <fillbert.susanto@gdn-commerce.com>
2024-03-29 12:07:46 +0700

    firebase config added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { KeysPipe } from './pipe/keys.pipe';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4815XN4YxsnwapctCLIMgSyPOicU2_Y8",
+  authDomain: "reykjavik-47907.firebaseapp.com",
+  projectId: "reykjavik-47907",
+  storageBucket: "reykjavik-47907.appspot.com",
+  messagingSenderId: "43955062786",
+  appId: "1:43955062786:web:9f5763c18c045c10147f1b"
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeysPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [
     provideClientHydration()
