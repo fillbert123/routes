96a09fd3a6f1df06b0f7256fc5f6b5fe6c8b9886
Fillbert <fillbert.susanto@gdn-commerce.com>
2025-01-21 21:03:04 +0700

    removing unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,9 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
 import { KeysPipe } from './pipe/keys.pipe';
-import { CorridorListComponent } from './component/corridor-list/corridor-list.component';
-import { CorridorIconComponent } from './component/corridor-icon/corridor-icon.component';
-import { BottomsheetComponent } from './component/bottomsheet/bottomsheet.component';
 import { firebaseConfig } from './firebase-config';
 import { TerminusSelectionComponent } from './component/terminus-selection/terminus-selection.component';
-import { LineDetailComponent } from './component/line-detail/line-detail.component';
-import { ButtonComponent } from './component/button/button.component';
 import { LoadingComponent } from './component/loading/loading.component';
-import { SvgPlaygroundComponent } from './svg-playground/svg-playground.component';
 import { SystemMapComponent } from './component/system-map/system-map.component';
 import { SidePanelComponent } from './component/side-panel/side-panel.component';
 import { ListComponent } from './component/list/list.component';
@@ -29,7 +23,6 @@ import { CorridorDetailComponent } from './page/corridor-detail/corridor-detail.
 import { ChipComponent } from './component/chip/chip.component';
 import { IconComponent } from './component/icon/icon.component';
 import { StopDetailComponent } from './page/stop-detail/stop-detail.component';
-import { SelectComponent } from './component/select/select.component';
 import { RoutePreviewComponent } from './component/route-preview/route-preview.component';
 import { LineOverviewComponent } from './component/line-overview/line-overview.component';
 import { TrackOverviewComponent } from './component/track-overview/track-overview.component';
@@ -40,14 +33,8 @@ import { BannerComponent } from './component/banner/banner.component';
   declarations: [
     AppComponent,
     KeysPipe,
-    CorridorListComponent,
-    CorridorIconComponent,
-    BottomsheetComponent,
     TerminusSelectionComponent,
-    LineDetailComponent,
-    ButtonComponent,
     LoadingComponent,
-    SvgPlaygroundComponent,
     SystemMapComponent,
     SidePanelComponent,
     ListComponent,
@@ -59,7 +46,6 @@ import { BannerComponent } from './component/banner/banner.component';
     ChipComponent,
     IconComponent,
     StopDetailComponent,
-    SelectComponent,
     RoutePreviewComponent,
     LineOverviewComponent,
     TrackOverviewComponent,
