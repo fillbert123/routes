5c7c82b8879b67131f96942c054d5c747803568a
Fillbert <fillbert.susanto@gdn-commerce.com>
2025-05-10 11:30:37 +0700

    feat: add firestore service

--- src/app/shared/methods.ts
+++ src/app/shared/methods.ts
@@ -1,7 +1,27 @@
-import corridorData from '../../assets/data/corridor.json';
-import stopData from '../../assets/data/stop.json';
-import stopDirection from '../../assets/data/stopDirection.json';
+import { FirebaseService } from '../service/firebase.service';
 
 export function isMobile() {
   return window.innerWidth < 480;
+}
+
+export function addDataToFirebase(data: any, collection: string, firebaseService: FirebaseService) {
+  data.forEach((item) => {
+    firebaseService.addData(collection, item, item.id).then(() => {
+      console.log(item.id + ' added to ' + collection + ' collection');
+    }).catch((error) => {
+      console.log(item.id + ' error when added to ' + collection + ' collection: ', error);
+    })
+  })
+}
+
+export function getDataFromFirebase(collection: string, firebaseService: FirebaseService) {
+  firebaseService.getAllData(collection).then((response) => {
+    console.log('response', response);
+  })
+}
+
+export function getDataFromDatabaseByKey(key: string, value: string, collection: string, firebaseService: FirebaseService) {
+  firebaseService.getDataByKey(collection, key, value).then((response) => {
+    console.log('response', response);
+  })
 }
