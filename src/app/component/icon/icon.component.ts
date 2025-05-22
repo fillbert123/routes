972815a7adf9e99f28112d353b5160caef4175e1
Fillbert <fillbert.susanto@gdn-commerce.com>
2025-05-22 20:45:45 +0700

    feat: inactive corridor

--- src/app/component/icon/icon.component.ts
+++ src/app/component/icon/icon.component.ts
@@ -9,8 +9,29 @@ export class IconComponent {
   @Input() color: string;
   @Input() icon: string;
   @Input() type: string;
+  @Input() isActive: boolean = true;
 
   getCorridorColor(color: string) {
     return `var(--${color})`
+  }
+
+  getCorridorStyleSmall() {
+    return {
+      'background-color': this.getCorridorColor(this.color),
+      'box-sizing': 'border-box', 
+      'border-style': (this.isActive) ? 'none' : 'solid', 
+      'border-width': '2px', 
+      'border-color': 'var(--grey)'
+    }
+  }
+
+  getCorridorStyleLarge() {
+    return {
+      'background-color': this.getCorridorColor(this.color),
+      'box-sizing': 'border-box', 
+      'border-style': (this.isActive) ? 'none' : 'solid', 
+      'border-width': '4px', 
+      'border-color': 'var(--grey)'
+    }
   }
 }
